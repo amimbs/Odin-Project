871cc59f99c3b885c7bda9052ad969c84b65a8d1
Andrew Mimbs <amimbs@costargroup.com>
2024-08-13 13:13:09 -0400

    feat(ui): add display of bookdata

--- full-stack-javascript/javascript/objects-and-object-constructors/project-library/index.js
+++ full-stack-javascript/javascript/objects-and-object-constructors/project-library/index.js
@@ -4,22 +4,46 @@ class Book {
         this.author = author;
         this.pages = pages;
         this.read = read;
-
-        this.info = function () {
-            const readStatus = this.read ? "read" : "not read yet";
-            return `${this.title} by ${this.author}, ${this.pages}, ${readStatus}`;
-        };
     }
 }
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
-console.log(theHobbit.info());
 
-// const myLibrary = [];
+class Libaray {
+    constructor(books) {
+        this.books = books;
+    }
+}
 
-// function Book() {
-//   // the constructor...
-// }
+// seeder data 
+const dummyBooks = [
+    new Book('The Hobbit', 'J.R.R. Tolkien', 295, true),
+    new Book('1984', 'George Orwell', 328, false),
+    new Book('To Kill a Mockingbird', 'Harper Lee', 281, true),
+    new Book('Moby Dick', 'Herman Melville', 635, false)
+]
 
-// function addBookToLibrary() {
-//   // do stuff here
-// }
+const myLibrary  = new Libaray(dummyBooks);
+
+function displayLibrary() {
+    let libraryContainer = document.getElementById('library-container');
+    
+    myLibrary.books.forEach((book) => {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+        bookCard.innerHTML += `
+            <h2>${book.title}</h2>
+            <h3>${book.author}</h3>
+            <p>${book.pages}</p>
+            <p>${book.read}</p>
+            <button>Read Status</button>
+        `
+        libraryContainer.appendChild(bookCard);
+    })
+}
+
+function openForm() {
+    document.getElementById("newBook-Form").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("newBook-Form").style.display = "none";
+  }
